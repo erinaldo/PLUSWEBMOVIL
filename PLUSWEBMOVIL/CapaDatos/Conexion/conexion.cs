7b031bd45ce0c29f0f5c220dfd182d4849e49121
Margarita <Margarita@PLUSDESA>
2019-05-01 15:14:52 -0500

    Site Master

---
+++ PLUSWEBMOVIL/CapaDatos/Conexion/conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CapaDatos
+{
+    public class conexion
+    {
+        public void genearConexion()
+        {
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+            cn.Open();
+        }
+
+    }
+}
