8c96b56276522a2295c83dfda2d082d92b034cb8
Orlando <Java@PLUSDESA>
2019-05-20 17:01:58 -0500

    fe

---
+++ PLUSWEBMOVIL/CapaWeb/GenerarPDF/FacturaElectronica/PdfFacturaElectronica.cs
@@ -0,0 +1,25 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace CapaWeb.GenerarPDF.FacturaElectronica
+{
+    public class PdfFacturaElectronica
+    {
+        public string generarPdf()
+        {
+            string bpathPdfGenrado = "";
+
+
+
+
+
+
+            return bpathPdfGenrado;
+        }
+    }
+}
