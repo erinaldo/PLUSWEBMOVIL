e94f955409c6b79e697817bdb32f4846c5c83487
Margarita <Margarita@PLUSDESA>
2019-07-04 12:51:23 -0500

    bancos

--- PLUSWEBMOVIL/CapaProceso/Consultas/ConsultaBancos.cs
+++ PLUSWEBMOVIL/CapaProceso/Consultas/ConsultaBancos.cs
@@ -12,10 +12,10 @@ namespace CapaProceso.Consultas
     public class ConsultaBancos
     {
         CuentasBanco consultaBanco = new CuentasBanco();
-        public List<modelobancos> BuscartaBancos(string usuario, string cod_emp)
+        public List<modelobancos> BuscartaBancos(string usuario, string cod_emp, string banco, string tipo, string cuenta, string imprime)
         {
             List<modelobancos> lista = new List<modelobancos>();
-            lista = consultaBanco.ConsultaBancos(usuario,cod_emp);           
+            lista = consultaBanco.ConsultaBancos(usuario,cod_emp, banco,  tipo,  cuenta,  imprime);           
             return lista;
         }
     }
