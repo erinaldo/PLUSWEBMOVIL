4a1d36f7c0a18a3842e845368f5334ce40c992df
Margarita <Margarita@PLUSDESA>
2020-04-06 07:46:45 -0500

    Final

---
+++ PLUSWEBMOVIL/CapaProceso/Comfiar/ComfiarEsquema.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapaProceso.Comfiar
+{
+   public  class ComfiarEsquema
+    {
+       
+
+    }
+    public class Invoice
+    {
+        public  UBLExtensions Cabecera { get; set;}
+    }
+    public class UBLExtensions
+    {
+
+    }
+    public class UBLExtension
+    {
+       // public ExtensionContent
+    }
+}
